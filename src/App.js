f75c32cbc1ddef2a1e4dc6e5e7de3c8f1fc97d12
Armando Wilmans <armandowjunior@gmail.com>
2021-01-19 11:45:47 -0300

    Add react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'typeface-roboto';
 import { withStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
@@ -12,61 +12,48 @@ const styles = {
   }
 };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quotes: [],
-      selectedQuoteIndex: null,
-    }
-    this.generateNewQuoteIndex = this.generateNewQuoteIndex.bind(this); // For every method that uses this, we have to bind it;
-    // Or use ES7 syntax;
-    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
-  }
+function App({classes}) {
+  
+  const [quotes, setQuotes] = useState([]);
+  const [selectedQuoteIndex, setSelectedQuoteIndex] = useState(null);
 
-  componentDidMount() { // After component is mounted
-    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
-    .then(data => data.json()) // parsed the data
-    .then(quotes => this.setState({ quotes: quotes }, this.assignNewQuoteIndex)); // could just use quotes // // Use a callback function here to define the state. Since this is assynchronous, if we put the function later in the code, it will just get an empty state.
-  } // Not calling the function (the return result of it), just passing it as an callback;
+  useEffect( async () => {
+    const data = await fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+    const quotes = await data.json();
+    setQuotes(quotes);
+    setSelectedQuoteIndex(Math.floor(Math.random() * quotes.length))
+  }, []);
 
-  get selectedQuote() { // the get makes that the function may be called as an variable
-    if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+  function getSelectedQuote() { // the get makes that the function may be called as an variable
+    if (!quotes.length || !Number.isInteger(selectedQuoteIndex)) {
       return undefined;
     }
-    return this.state.quotes[this.state.selectedQuoteIndex];
+    return quotes[selectedQuoteIndex];
   }
 
-  /**
-   * Returns an integer representing an index in state.quotes
-   * if state.quotes is empty, returns undefined
-   */
-
-  generateNewQuoteIndex() {
-    if (!this.state.quotes.length) {
+  function generateNewQuoteIndex() {
+    if (!quotes.length) {
       return undefined;
     }
-    return Math.floor(Math.random() * this.state.quotes.length); // could use lodash
+    return Math.floor(Math.random() * quotes.length); // could use lodash
   }
 
-  assignNewQuoteIndex() {
-    this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex() }) 
+  function assignNewQuoteIndex() {
+    setSelectedQuoteIndex(generateNewQuoteIndex());
   }
 
   // this.props.classes.container - the classes props gets filled with the styles;
-  render() {
-    return (
-      <Grid className={this.props.classes.container} id="quote-box" justify="center" container> 
-        <Grid xs={11} lg={8} item>
-          {
-            this.selectedQuote ?
-          <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} /> 
-          : null
-          }
-        </Grid>
+  return (
+    <Grid className={classes.container} id="quote-box" justify="center" container> 
+      <Grid xs={11} lg={8} item>
+        {
+          getSelectedQuote() ?
+        <QuoteMachine selectedQuote={getSelectedQuote()} assignNewQuoteIndex={assignNewQuoteIndex} /> 
+        : null
+        }
       </Grid>
-    );
-  }
+    </Grid>
+  );
 }
 
 export default withStyles(styles)(App); // To get the App component filled with the styles;
